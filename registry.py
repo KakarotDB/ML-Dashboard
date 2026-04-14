f29860370afef7248949844a6120f6516cbe0cb3
bristitalukdar17-pixel <bristitalukdar17@gmail.com>
2026-04-14 14:49:14 +0530

    [ADDED] implemented binning, regression, and clustering smoothing module

--- registry.py
+++ registry.py
@@ -2,7 +2,7 @@ from pipeline import PreprocessingPipeline
 from modules.discretization.discretization import Discretization
 from modules.data_reduction.data_reduction import DataReduction, Histogram
 from modules.similarity.similarity import SimilarityAnalyzer
-
+from modules.smoothing.smoothing import DataSmoother
 # ------------------------------------------------------------------
 # Pattern for each:
 #   from modules.<folder>.<file> import <ClassName>
@@ -12,7 +12,7 @@ from modules.similarity.similarity import SimilarityAnalyzer
 PreprocessingPipeline.register("Discretization", Discretization)
 
 # PreprocessingPipeline.register("Missing Values", MissingValueEstimator)
-# PreprocessingPipeline.register("Smoothing", Smoothing)
+PreprocessingPipeline.register("Data Smoothing", DataSmoother)
 PreprocessingPipeline.register("Data Reduction", DataReduction)
 # PreprocessingPipeline.register("Similarity", Similarity)
 
